96bcc4ffed67de6134660d8930a8616d24a76287
abel armando <abelarmando79@gmail.com>
2025-04-28 11:21:36 +0700

    membenarkan

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,6 @@ function App() {
       <Technologies />
       <Projects />
       <Contact />
-      <Notif text="Sending Email..." bg="bg-black/50">
-        <Flex align="center" gap="middle">
-          <Spin />
-        </Flex>
-      </Notif>
     </div>
   );
 }
